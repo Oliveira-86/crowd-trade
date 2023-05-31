736466ee226240546cc25b4a11d5090eca8357a3
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-05-31 20:02:25 -0300

    core: reusable input component

---
+++ components/Input.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import Feather from '@expo/vector-icons/Feather'
+import clsx from 'clsx'
+
+const Input = ({ label, eyes }, props) => {
+  const [isSecureEntry, setIsSecureEntry] = useState(props.isSecureEntry)
+
+  useEffect(() => {
+    setIsSecureEntry(props.isSecureEntry)
+  }, [props.isSecureEntry])
+
+  return (
+    <>
+      <Text className="font-sora w-full text-xs decoration-0 mb-1 text-gray_default">
+        {label}
+      </Text>
+      <View className="w-full rounded-md bg-gray_light h-[50px]">
+        <TextInput
+          {...props}
+          className="py-2 px-3 text-base font-sora text-gray_default"
+          secureTextEntry={isSecureEntry}
+          bigRight={props.bigRight}
+          onFocus={props.onFocus}
+          placeholderTextColor="#A0A0A0"
+          autoCapitalize={props.autoCapitalize || 'none'}
+          multiline={props.multiline}
+        />
+
+        {eyes && (
+          <View className="absolute right-4 top-4">
+            <TouchableOpacity onPress={() => setIsSecureEntry(!isSecureEntry)}>
+              {!isSecureEntry ? (
+                <Feather name="eye-off" size={20} color="#A0A0A0" />
+              ) : (
+                <Feather name="eye" size={20} color="#A0A0A0" />
+              )}
+            </TouchableOpacity>
+            {props.errorMessage && (
+              <Text className="absolute bottom-[-22px] text-red-600 text-xs font-sora mt-1">
+                {props.errorMessage}
+              </Text>
+            )}
+          </View>
+        )}
+      </View>
+    </>
+  )
+}
+
+export default Input
