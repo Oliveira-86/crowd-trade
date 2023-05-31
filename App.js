518438af006f04ebcc5a03630c5ddc2d5c0c8de9
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-05-31 14:34:15 -0300

    core: styles setup

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+import theme from './src/global/styles/theme'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback } from 'react'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Sora-Bold': require('./assets/fonts/Sora-Bold.ttf'),
+    'Sora-Regular': require('./assets/fonts/Sora-Regular.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <ThemeProvider theme={theme}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <Text style={{ fontFamily: 'Sora-Bold', fontSize: 20 }}>
+          Hello World!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container: { flex: 1 },
+})
---
+++ global/styles/theme.js
@@ -0,0 +1,30 @@
+import {} from '../../lib/responsiveFontSize'
+
+export default {
+  colors: {
+    primary: '#770FDF',
+    secondary: '#0FDF8F',
+    bg_primary: '#F7EFFF',
+    black: '#000',
+    white: '#FFF',
+    gray_light: '#F4F4F4',
+    gray: '#CFCFCF',
+    gray_strong: '#A0A0A0',
+    error: '#DC4343',
+    success: '#4AAB65',
+  },
+
+  fonts: {
+    regular: 'Sora-Regular',
+    bold: 'Sora-Bold',
+  },
+
+  fontSize: {
+    xmini: normalize(10),
+    mini: normalize(12.5),
+    small: normalize(15),
+    medium: normalize(16),
+    large: normalize(20),
+    xlarge: normalize(24),
+  },
+}
