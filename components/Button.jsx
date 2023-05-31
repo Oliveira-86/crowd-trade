6d26ecc6b36ae0c877c9a722445111c21d95cdd1
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-05-31 19:16:35 -0300

    core: reusable button component

---
+++ components/Button.jsx
@@ -0,0 +1,47 @@
+import { TouchableOpacity, Text } from 'react-native'
+import React from 'react'
+import clsx from 'clsx'
+
+const Button = ({ large, variant, label }) => {
+  return (
+    <TouchableOpacity
+      className={clsx(
+        `
+        relative 
+        rounded-md 
+        w-full 
+        font-sora_bold
+        px-3 
+        py-2 
+        h-[50px] 
+        flex 
+        items-center 
+        justify-center`,
+        large && 'p-3',
+        variant === 'primary' && 'bg-primary border-none',
+        variant === 'secondary' && 'bg-secondary border-none',
+        variant === 'outlined' &&
+          'bg-transparent border-[1px] border-gray_default',
+        variant === 'outlinedPrimary' && 'bg-bg_primary border-none'
+      )}
+    >
+      <Text
+        className={clsx(
+          `
+          text-sm
+          text-center
+          font-sora_bold
+          uppercase
+        `,
+          (variant === 'primary' || variant === 'secondary') && 'text-white',
+          (variant === 'outlined' || variant === 'outlinedPrimary') &&
+            'text-primary'
+        )}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+export default Button
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     './App.{js,jsx,ts,tsx}',
     './<custom directory>/**/*.{js,jsx,ts,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
-    './component/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -12,7 +12,7 @@ module.exports = {
         primary: '#770FDF',
         secondary: '#0FDF8F',
         black: '#000',
-        gray: '#A0A0A0',
+        gray_default: '#A0A0A0',
         gray_light: '#F4F4F4',
         white: '#FFF',
         bg_primary: '#F7EFFF',
