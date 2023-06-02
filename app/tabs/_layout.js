840b3bf6ff82cee7c24a8bbc27b240be472fc528
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-06-01 23:32:08 -0300

    core: icons tab customized

--- app/tabs/_layout.js
+++ app/tabs/_layout.js
@@ -1,11 +1,59 @@
 import { Tabs } from 'expo-router'
 
+import HomeIcon from '../../assets/icons/home.svg'
+import { Foundation, Ionicons, Feather } from '@expo/vector-icons'
+
 export default TabLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen name="Home" />
-      <Tabs.Screen name="Trade" />
-      <Tabs.Screen name="Portfolio" />
+    <Tabs screenOptions={{}}>
+      <Tabs.Screen
+        name="Home"
+        options={{
+          tabBarLabel: 'Home',
+          title: '',
+          tabBarActiveTintColor: '#770FDF',
+          tabBarInactiveTintColor: '#000',
+          tabBarIcon: ({ focused }) => (
+            <Foundation
+              name="home"
+              size={24}
+              color={focused ? '#770FDF' : 'black'}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Trade"
+        options={{
+          tabBarLabel: 'Trade',
+          title: '',
+          tabBarActiveTintColor: '#770FDF',
+          tabBarInactiveTintColor: '#000',
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="ios-swap-horizontal"
+              size={24}
+              color={focused ? '#770FDF' : 'black'}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Portfolio"
+        options={{
+          tabBarLabel: 'Portfolio',
+          title: '',
+          tabBarActiveTintColor: '#770FDF',
+          tabBarInactiveTintColor: '#000',
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="pie-chart"
+              size={24}
+              color={focused ? '#770FDF' : 'black'}
+            />
+          ),
+        }}
+      />
     </Tabs>
   )
 }
