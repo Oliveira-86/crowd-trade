b323d359afd008ae10c5e82d40888c356a3bd296
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-06-02 19:05:56 -0300

    core: create data to home screen

---
+++ actions/data.js
@@ -0,0 +1,22 @@
+export const userData = {
+  account: 1457.23,
+  currentValue: 1641.46,
+  yield: 31.83,
+  funds: [
+    {
+      name: 'Wind Fund',
+      value: 1032.25,
+      yield: 3.51,
+    },
+    {
+      name: 'Solar Fund',
+      value: 1032.25,
+      yield: -0.13,
+    },
+    {
+      name: 'Natural Fund',
+      value: 1032.25,
+      yield: 5.9,
+    },
+  ],
+}
