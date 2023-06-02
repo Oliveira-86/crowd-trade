0b7d0ea060691392fd38ded52596866992ac18e0
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-06-01 22:19:06 -0300

    core: validate login

--- app/signIn.jsx
+++ app/signIn.jsx
@@ -1,22 +1,64 @@
-import React from 'react'
-import { Link } from 'expo-router'
-import { View, Text } from 'react-native'
+import React, { useCallback, useState } from 'react'
+import { Link, useRouter } from 'expo-router'
+import { View, Text, ToastAndroid } from 'react-native'
 import Button from '../components/Button'
 import Input from '../components/Input'
+import { onValidateEmail, onValidatePassword } from '../lib/validation'
 
 const SignIn = () => {
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [hasPasswordError, setHasPasswordError] = useState('')
+  const [hasEmailError, setHasEmailError] = useState('')
+
+  const onEmailText = (text) => {
+    setEmail(text)
+    const res = onValidateEmail(text)
+    if (res.error) {
+      setHasEmailError(res.message)
+      return
+    }
+    setHasEmailError('')
+  }
+  const onPasswordText = (text) => {
+    setPassword(text)
+    const res = onValidatePassword(text)
+    if (res.error) {
+      setHasPasswordError(res.message)
+      return
+    }
+    setHasPasswordError(false)
+  }
+
+  const onSubmitSignIn = () => {
+    // router.push(/)
+  }
   return (
     <View className="flex-1 bg-white flex items-center py-5 px-5">
       <Text className="font-sora_bold text-lg mb-8">Login</Text>
-      <Input label="E-mail" isLoading size="small" />
       <Input
+        label="E-mail"
+        isLoading
+        size="small"
+        onChangeText={onEmailText}
+        errorMessage={hasEmailError}
+      />
+      <Input
+        value={password}
         label="Password"
         size="small"
-        style={{ marginTop: 20, marginBottom: 37 }}
+        style={{ marginTop: 22, marginBottom: 37 }}
         placeholder="Minimum 8 characters"
+        onChangeText={onPasswordText}
+        errorMessage={hasPasswordError}
         eyes
       />
-      <Button label="entrar" variant="primary" />
+      <Button
+        label="entrar"
+        variant="primary"
+        onPress={onSubmitSignIn}
+        disabled={hasEmailError || password.length < 8}
+      />
       <View className="flex flex-row items-center gap-1 mt-1 text-gray_default">
         <Text className="text-gray_default">Don’t have an account?</Text>
         <Link href="/signUp">
